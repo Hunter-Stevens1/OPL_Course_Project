ef7ddeb8dd1f4b9065c1e4d885da70951e80fd6a
Hunter-Stevens1 <hunter.stevens05@gmail.com>
2021-09-06 22:20:44 -0400

    Task 3 complete!

--- OPL_Project_HL/src/main/Main.java
+++ OPL_Project_HL/src/main/Main.java
@@ -1,5 +1,8 @@
 package main;
 
+import java.io.*;
+import java.util.Random;
+
 interface JExpr{
     public String pp();
 }
@@ -39,9 +42,42 @@ class JMult implements JExpr{
 
 
 public class Main {
-    
+	
+	//test suite for J0 programs
+	public static void ppTest() {
+		
+		Random rand = new Random();
+		
+		JNum jnum1 = new JNum(1);
+		JNum jnum2 = new JNum(1);
+		
+		for(int i = 1; i <= 12; i++) {
+			jnum1.n = rand.nextInt(1000);
+			jnum2.n = rand.nextInt(1000);
+			
+			if(i % 4 == 1 && i != 1) {
+				System.out.println();
+			}
+			if (i <= 4) {
+				System.out.println("PPTest " + i + ": Expected: " + jnum1.n + " Output: " + jnum1.pp());
+			}
+			if (i > 4 && i <=8) {
+				JPlus jplus = new JPlus(jnum1, jnum2);
+				System.out.println("PPTest " + i + ": Expected: (+ " + jnum1.n + " " + jnum2.n + ") Output: " + jplus.pp());
+			}
+			if (i > 8 && i <=12) {
+				JMult jmult = new JMult(jnum1, jnum2);
+				System.out.println("PPTest " + i + ": Expected: (* " + jnum1.n + " " + jnum2.n + ") Output: " + jmult.pp());
+			}
+			
+		}
+		
+		
+	}
+	
+	
     public static void main(String[] args) {
-        
+        ppTest();
     }
     
 }
